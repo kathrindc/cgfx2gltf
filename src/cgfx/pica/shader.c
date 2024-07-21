57eb8c3593bb6836416c796be8090621e403fe31
Kathrin De Cecco <kad@toast.ws>
2024-07-21 09:41:51 +0200

    Adjust macro names in shader.h and add more pica_cmdr methods

--- src/cgfx/pica/shader.c
+++ src/cgfx/pica/shader.c
@@ -181,3 +181,126 @@ stencil_operation pica_cmdr_get_stencil_test(pica_command_reader *reader) {
 
   return result;
 }
+
+#define PICK_COMMAND_FOR_TEX_UNIT(var, cmd)                                    \
+  switch (unit) {                                                              \
+  case PICA_TEX_UNIT0:                                                         \
+    var = reader->command_buffer[PICA_CMD_TEX_UNIT0_##cmd];                    \
+    break;                                                                     \
+  case PICA_TEX_UNIT1:                                                         \
+    var = reader->command_buffer[PICA_CMD_TEX_UNIT1_##cmd];                    \
+    break;                                                                     \
+  case PICA_TEX_UNIT2:                                                         \
+    var = reader->command_buffer[PICA_CMD_TEX_UNIT2_##cmd];                    \
+    break;                                                                     \
+  }
+
+uint32_t pica_cmdr_get_tex_unit_address(pica_command_reader *reader,
+                                        uint8_t unit) {
+  assert(reader->is_valid);
+  assert(unit >= PICA_TEX_UNIT0 && unit <= PICA_TEX_UNIT2);
+
+  uint32_t param;
+  PICK_COMMAND_FOR_TEX_UNIT(param, ADDRESS);
+  return param;
+}
+
+texture_mapping pica_cmdr_get_tex_unit_mapper(pica_command_reader *reader,
+                                              uint8_t unit) {
+  assert(reader->is_valid);
+  assert(unit >= PICA_TEX_UNIT0 && unit <= PICA_TEX_UNIT2);
+
+  texture_mapping result;
+  uint32_t param;
+  PICK_COMMAND_FOR_TEX_UNIT(param, PARAM);
+
+  result.mag_filter = (param >> 1) & 1;
+  result.min_filter = ((param >> 2) & 1) | ((param >> 23) & 2);
+  result.wrap_u = ((param >> 12) & 0xF);
+  result.wrap_v = ((param >> 8) & 0xF);
+  result.min_lod = 0;
+  result.lod_bias = 0;
+  result.border_colour = 0;
+
+  return result;
+}
+
+uint32_t pica_cmdr_get_tex_unit_border_colour(pica_command_reader *reader,
+                                              uint8_t unit) {
+  assert(reader->is_valid);
+  assert(unit >= PICA_TEX_UNIT0 && unit <= PICA_TEX_UNIT2);
+
+  uint32_t param;
+  PICK_COMMAND_FOR_TEX_UNIT(param, BORDER_COLOUR);
+
+  return ((param & 0xFF) << 24) | ((param & 0xFF00) << 8) |
+         ((param & 0xFF0000) >> 8) | ((param >> 24) & 0xFF);
+}
+
+size2d pica_cmdr_get_tex_unit_size(pica_command_reader *reader, uint8_t unit) {
+  assert(reader->is_valid);
+
+  size2d result;
+  uint32_t param;
+  PICK_COMMAND_FOR_TEX_UNIT(param, SIZE);
+
+  result.x = (param >> 16) & 0xFFFF;
+  result.y = param & 0xFFFF;
+
+  return result;
+}
+
+texture_combiner pica_cmdr_get_tev_stage(pica_command_reader *reader, uint8_t stage) {
+  assert(reader->is_valid);
+  assert(stage >= PICA_TEV_STAGE0 && stage <= PICA_TEV_STAGE5);
+
+  texture_combiner result;
+  uint16_t base_command = 0;
+
+  switch (stage) {
+  case PICA_TEV_STAGE0: base_command = PICA_CMD_TEV_STAGE0_SOURCE; break;
+  case PICA_TEV_STAGE1: base_command = PICA_CMD_TEV_STAGE1_SOURCE; break;
+  case PICA_TEV_STAGE2: base_command = PICA_CMD_TEV_STAGE2_SOURCE; break;
+  case PICA_TEV_STAGE3: base_command = PICA_CMD_TEV_STAGE3_SOURCE; break;
+  case PICA_TEV_STAGE4: base_command = PICA_CMD_TEV_STAGE4_SOURCE; break;
+  case PICA_TEV_STAGE5: base_command = PICA_CMD_TEV_STAGE5_SOURCE; break;
+  }
+
+  uint32_t source = reader->command_buffer[base_command];
+  result.rgb_source[0] = source & 0xF;
+  result.rgb_source[1] = (source >> 4) & 0xF;
+  result.rgb_source[2] = (source >> 8) & 0xF;
+  result.alpha_source[0] = (source >> 16) & 0xF;
+  result.alpha_source[1] = (source >> 20) & 0xF;
+  result.alpha_source[2] = (source >> 24) & 0xF;
+
+  uint32_t operand = reader->command_buffer[base_command + 1];
+  result.rgb_operand[0] = operand & 0xF;
+  result.rgb_operand[1] = (operand >> 4) & 0xF;
+  result.rgb_operand[2] = (operand >> 8) & 0xF;
+  result.alpha_operand[0] = (operand >> 12) & 0xF;
+  result.alpha_operand[1] = (operand >> 16) & 0xF;
+  result.alpha_operand[2] = (operand >> 20) & 0xF;
+
+  uint32_t combine = reader->command_buffer[base_command + 2];
+  result.rgb_operator = combine & 0xFFFF;
+  result.alpha_operator = combine >> 16;
+
+  uint32_t scale = reader->command_buffer[base_command + 4];
+  result.rgb_scale = (scale & 0xFFFF) + 1;
+  result.alpha_scale = (scale >> 16) + 1;
+
+  return result;
+}
+
+alpha_test pica_cmdr_get_alpha_test(pica_command_reader *reader) {
+  assert(reader->is_valid);
+
+  alpha_test result;
+  uint32_t param = reader->command_buffer[PICA_CMD_ALPHA_TEST_CONFIG];
+  result.is_enabled = param & 1;
+  result.function = (param >> 4) & 0xF;
+  result.reference = (param >> 8) & 0xFF;
+
+  return result;
+}
